2795e560f12a39dd54f0b7eb0d51fbae3ac4e62d
Joey3011 <jeff.me.7919@gmail.com>
2022-11-29 04:13:37 +0800

    initial commit

--- script.js
+++ script.js
@@ -74,6 +74,42 @@ const quoteBank = [
     {
       quote: "First, solve the problem. Then, write the code.",
       author: "John Johnson"
+    },
+    {
+        quote: "Programming isn't about what you know; it's about what you can figure out.",
+        author: "Chris Pine"
+    },
+    {
+      quote: "The only way to learn a new programming language is by writing programs in it.",
+      author: "Dennis Ritchie"
+    },
+    {
+        quote: "Sometimes it's better to leave something alone, to pause, and that's very true of programming.",
+        author: "Joyce Wheeler"
+    },
+    {
+      quote: "In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them.",
+      author: "Andrew Hunt"
+    },
+    {
+      quote: "Remember that code is really the language in which we ultimately express the requirements. We may create languages that are closer to the requirements. We may create tools that help us parse and assemble those requirements into formal structures. But we will never eliminate necessary precision—so there will always be code.",
+      author: "Robert C. Martin"
+    },
+    {
+        quote: "The computer programmer is a creator of universes for which he alone is the lawgiver. No playwright, no stage director, no emperor, however powerful, has ever exercised such absolute authority to arrange a stage or field of battle and to command such unswervingly dutiful actors or troops.",
+        author: "Joseph Weizenbaum"
+    },
+    {
+      quote: "The best programs are written so that computing machines can perform them quickly and so that human beings can understand them clearly. A programmer is ideally an essayist who works with traditional aesthetic and literary forms as well as mathematical concepts, to communicate the way that an algorithm works and to convince a reader that the results will be correct.",
+      author: "Donald E. Knuth, Selected Papers on Computer Science"
+    },
+    {
+        quote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        author: "Martin Fowler"
+    },
+    {
+      quote: "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
+      author: "John Woods"
     }
 ]
 
